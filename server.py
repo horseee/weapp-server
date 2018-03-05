69352ee9b39742733406d9a944cdf0ee9ce0ab92
horseee <www.maxinyin@qq.com>
2018-03-05 17:02:23 +0800

    add the upload and post part

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
+# encoding=utf8
 from flask import Response
 from flask import Flask, jsonify
 from flask import abort
 from flask import request, make_response
-from flask import url_for
+from flask import url_for, render_template
+from werkzeug.utils import secure_filename
 import pymysql
 import requests
+import os
 
 app = Flask(__name__)
 db= pymysql.connect(host="localhost",user="root", password="maxinyin",db="loreal",port=3306,charset='utf8mb4')
@@ -24,6 +27,29 @@ def streamwav():
 	    	id = id + 1
     return Response(generate(), mimetype="audio/x-wav")
 
+@app.route("/new-post", methods = ['Post'])
+def create_Post():
+    sql_count = "select * from PostCount"
+    cur.execute(sql_count)
+    Next_ID = (cur.fetchone())[0]+1
+    print("NextPostID: "+ str(Next_ID))
+
+    url_list = request.json['image']
+    if len(url_list) < 3:
+        for i in range(3 - len(url_list)):
+            url_list.append('')
+    print(url_list)
+
+    sql_insert = "insert into Post(PostID, Title, Detail, ImageUrl_0, ImageUrl_1,ImageUrl_2, UserID, LikeCount) values ('" + str(Next_ID) + "','" + request.json['title'] + "','" + request.json['detail'] + "','" + url_list[0] + "','" + url_list[1] + "','" + url_list[2] + "','" + str(request.json['id']) + "','0')"
+    status = cur.execute(sql_insert)
+    if status == 1:
+        sql_update = "update PostCount set num = '"+ str(Next_ID) +"'"
+        cur.execute(sql_update)
+        db.commit()
+        return str(Next_ID), 201
+    else:
+        return 'sql insert fail', 201
+
 @app.route("/login", methods = ['POST'])
 def create_User():
 	sql_count = "select * from UserCount"
@@ -31,7 +57,7 @@ def create_User():
 	Next_ID = (cur.fetchone())[0]+1
 	print("NextID: "+ str(Next_ID))
 
-	url = "https://api.weixin.qq.com/sns/jscode2session?" + "appid=wx4963f019e8e86a05&" + "secret=7474d82965d28cee0e05136c92ea0cb0&" + "grant_type=authorization_code&" + "js_code=" + request.json['LoginCode']
+	url = "https://api.weixin.qq.com/sns/jscode2session?" + "appid=..." + "secret=...&" + "grant_type=authorization_code&" + "js_code=" + request.json['LoginCode']
 	r = requests.get(url)
 	openid_str = r.content.decode()
 	start_pos = openid_str.find("openid") + 9
@@ -55,7 +81,7 @@ def create_User():
     # return jsonify({'task': task}), 201
     # 
     # 
-@app.route("/image/<int:imageid>.jpg")
+   @app.route("/image/<int:imageid>.jpg")
 def get_image(imageid):
     image = open("image/{}.jpg".format(imageid), "rb")
     resp = Response(image, mimetype="image/jpeg")
@@ -63,25 +89,37 @@ def get_image(imageid):
 
 @app.route("/question/<time>")
 def get_Question(time):
-	questions = []
+        questions = []
 
-	sql_count = "select * from QuestionList where useDate = '" + time +"'"
-	cur.execute(sql_count) 
+        sql_count = "select * from QuestionList where useDate = '" + time +"'"
+        cur.execute(sql_count)
 
-	for i in range(4): 
-		QueInf = cur.fetchone()
-		question = {
-			'id':QueInf[6],
-			'detail': QueInf[0],
-			'A': QueInf[1],
-			'B': QueInf[2],
-			'C': QueInf[3],
-			'D': QueInf[4],
-			'time': QueInf[5],
-		}
-		questions.append(question)
-	print(questions)
-	return jsonify({'questions': questions}), 201
+        for i in range(4):
+                QueInf = cur.fetchone()
+                question = {
+                        'id':QueInf[6],
+                        'detail': QueInf[0],
+                        'A': QueInf[1],
+                        'B': QueInf[2],
+                        'C': QueInf[3],
+                        'D': QueInf[4],
+                        'time': QueInf[5],
+                }
+                questions.append(question)
+        print(questions)
+        return jsonify({'questions': questions}), 201
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    print(request)
+    if request.method == 'POST':
+        f = request.files['file']
+        basepath = os.path.dirname(__file__)
+        upload_path = os.path.join(basepath,r'uploads',secure_filename(f.filename))
+        f.save(upload_path)
+        print(upload_path)
+        return upload_path ,200
+    return 'fail', 404
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000, debug=True)
